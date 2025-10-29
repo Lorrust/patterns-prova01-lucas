8cb3856404b54d2760afdf6be889bf9de262b461
Lucas Siva <lucasrs202@gmail.com>
2025-10-29 20:21:22 -0300

    Finish second exam, implementing bridge and adapter patterns and update pipeline

--- desafio02/index01.js
+++ desafio02/index01.js
@@ -1,1 +1,45 @@
-console.log("Hello, World! 001");
+// Adapter Pattern
+
+class LegacyPaymentSystem {
+  makePayment(amount) {
+    console.log(`Pagando R$${amount} com sistema legado.`);
+  }
+}
+
+class ModernPaymentAPI {
+  process(amountInCents) {
+    console.log(`Pagamento de R$${amountInCents / 100} via API moderna.`);
+  }
+}
+
+class ModernPaymentAdapter {
+  constructor(modernAPI) {
+    this.modernAPI = modernAPI;
+  }
+
+  makePayment(amountInCents) {
+    const amount = amountInCents * 100;
+    this.modernAPI.process(amount);
+  }
+}
+
+class PaymentProcessor {
+  constructor(system) {
+    this.system = system;
+  }
+
+  pay(amount) {
+    this.system.makePayment(amount);
+  }
+}
+
+// Cliente
+
+const legacy = new LegacyPaymentSystem();
+const legacyProcessor = new PaymentProcessor(legacy);
+legacyProcessor.pay(100);
+
+const modernAPI = new ModernPaymentAPI();
+const adaptedAPI = new ModernPaymentAdapter(modernAPI);
+const modernProcessor = new PaymentProcessor(adaptedAPI);
+modernProcessor.pay(250);
--- desafio02/index02.js
+++ desafio02/index02.js
@@ -1,1 +1,57 @@
-console.log("Hello, World! 002");
+// Bridge Pattern
+
+class TV {
+  turnOn() {
+    console.log("TV ligada.");
+  }
+
+  turnOff() {
+    console.log("TV desligada.");
+  }
+}
+
+class SamsungTV extends TV {
+  turnOn() {
+    console.log("TV Samsung ligada.");
+  }
+
+  turnOff() {
+    console.log("TV Samsung desligada.");
+  }
+}
+
+class SonyTV extends TV {
+  turnOn() {
+    console.log("TV Sony ligada.");
+  }
+
+  turnOff() {
+    console.log("TV Sony desligada.");
+  }
+}
+
+class RemoteControl {
+  constructor(tv) {
+    this.tv = tv;
+  }
+
+  turnOn() {
+    this.tv.turnOn();
+  }
+
+  turnOff() {
+    this.tv.turnOff();
+  }
+}
+
+// Cliente
+
+const samsung = new SamsungTV();
+const samsungRemote = new RemoteControl(samsung);
+samsungRemote.turnOn();
+samsungRemote.turnOff();
+
+const sony = new SonyTV();
+const sonyRemote = new RemoteControl(sony);
+sonyRemote.turnOn();
+sonyRemote.turnOff();
